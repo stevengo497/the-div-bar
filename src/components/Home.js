fa94e61d337340ad7df4433c961733fe6a0c8c5c
“stevengo497” <ngo.steve497@yahoo.com>
2018-05-08 14:36:27 -0600

    images showing on homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,19 @@
 import React, { Component } from 'react';
 
+
 class Home extends Component {
     render () {
         return (
             <div>
                 <h1>Homepage</h1>
+                <div id="carouselExampleSlidesOnly" class="carousel slide" data-ride="carousel"/>
+                  <div class="carousel-inner"/>
+                    <div class="carousel-item active"/>
+                      <img class="d-block w-100" src={('./images/barimage.jpg')} alt="First slide"/>
+                    <div class="carousel-item"/>
+                      <img class="d-block w-100" src={('./images/garnisheddrink.jpg')} alt="Second slide"/>
+                    <div class="carousel-item"/>
+                      <img class="d-block w-100" src={('./images/relax.jpg')} alt="Third slide"/>
             </div>
         )
     }
