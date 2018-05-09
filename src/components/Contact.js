725e0a73a95ada5ecbf40ab28d22629005aad2ba
“stevengo497” <ngo.steve497@yahoo.com>
2018-05-09 15:43:50 -0600

    form on contact list and links to form.io db, but need to get response email working

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import './Contact.css';
 
+
 class Contact extends Component {
     render () {
         return (
             <div>
                 <h1 class="line-1 anim-contact">Contact Us</h1>
-                <img id="contactPic"  src={('./images/relax.jpg')} />
+                <div id="formio"></div>
+                <img id="contactPic" src={('./images/relax.jpg')} />
             </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -5,7 +5,10 @@ import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import registerServiceWorker from './registerServiceWorker';
 
+
+
 ReactDOM.render((<BrowserRouter>
                     <App />
-                    </BrowserRouter>), document.getElementById('root'));
+                    </BrowserRouter>),
+                    document.getElementById('root'));
 registerServiceWorker();
