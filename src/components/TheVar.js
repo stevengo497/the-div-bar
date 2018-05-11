04473856bcba1121c557c97d4360c983ad413396
“stevengo497” <ngo.steve497@yahoo.com>
2018-05-11 09:55:32 -0600

    the var has pics set up, need to link to menu now

--- src/components/TheVar.js
+++ src/components/TheVar.js
@@ -1,12 +1,31 @@
 import React, { Component} from 'react';
 import '../styling/TheVar.css';
+import {Modal, Button, ButtonToolbar} from 'react-bootstrap';
 
 class TheVar extends Component {
+    constructor(props, context) {
+      super(props, context);
+
+      this.state = {
+        lgShow: false
+      };
+    }
     render () {
+        let lgClose = () => this.setState({ lgShow: false });
+        let largeClose = () => this.setState({ largeClose: false });
         return (
             <div>
                 <h1 className="line-1 anim-var">The Var</h1>
-                <img id="theVarPic"  src={('./images/newbar.jpeg')} />
+                <div className="specialityWrapper">
+                        <span className="specialityWord">Speciality Drinks</span>
+                        <img className="specialityPic"  src={('./images/garnisheddrink.jpg')} onClick={() => this.setState({ lgShow: true })}/>
+                    {/* <AppetizerMenu show={this.state.lgShow} onHide={lgClose} /> */}
+                </div>
+                <div className="aleWrapper">
+                        <span className="aleWord">ALEgorithms</span>
+                        <img className="alePic"  src={('./images/beer.jpg')} onClick={() => this.setState({ largeClose: true })}/>
+                    {/* <BoilerplateMenu show={this.state.largeClose} onHide={largeClose} /> */}
+                </div>
             </div>
         )
     }
