69d4267766a9225f1c3e4e613292123779d0fb65
“stevengo497” <ngo.steve497@yahoo.com>
2018-05-14 12:26:14 -0600

    new nav bar!

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,40 +1,64 @@
 import React, { Component} from 'react';
 import { Link } from 'react-router-dom';
-import {Navbar, Nav, NavItem} from 'react-bootstrap';
+// import {Navbar, Nav, NavItem} from 'react-bootstrap';
+import '../styling/Header.css';
 
 
 class Header extends Component { // also the Navigation Bar
     render () {
         return (
             <header>
-                <Navbar>
-                  <Navbar.Header>
-                    <Navbar.Brand>
-                      The &lt;div&gt; bar
-                  </Navbar.Brand>
-              </Navbar.Header>
-                  <Nav>
-                    <NavItem eventKey={1}>
-                      <Link to={"/"}>Home</Link>{" "}
 
-                    </NavItem>
-                    <NavItem eventKey={2}>
-                      <Link to={"/thevar"}>The Var</Link>{" "}
+                    <div id="container">
 
-                    </NavItem>
-                    <NavItem eventKey={3}>
-                      <Link to={"/boilerplates"}>Food Menu</Link>{" "}
+                          <Link to={"/"} id="homepageLink">Homepage</Link>{" "}
 
-                    </NavItem>
-                    <NavItem eventKey={5}>
-                      <Link to={"/contact"}>Contact</Link>{" "}
+                          <Link to={"/thevar"} id="theVarLink">The Var</Link>{" "}
 
-                    </NavItem>
-                  </Nav>
-              </Navbar>
+                          <Link to={"/boilerplates"} id="foodMenuLink">Food Menu</Link>{" "}
+
+                          <Link to={"/contact"} id="ContactLink">Contact</Link>{" "}
+                     </div>
+
             </header>
         )
     }
 }
 
 export default Header;
+
+// class Header extends Component { // also the Navigation Bar
+//     render () {
+//         return (
+//             <header>
+//                 <Navbar>
+//                   <Navbar.Header>
+//                     <Navbar.Brand>
+//                       The &lt;div&gt; bar
+//                   </Navbar.Brand>
+//               </Navbar.Header>
+//                   <Nav>
+//                     <NavItem eventKey={1}>
+//                       <Link to={"/"}>Home</Link>{" "}
+//
+//                     </NavItem>
+//                     <NavItem eventKey={2}>
+//                       <Link to={"/thevar"}>The Var</Link>{" "}
+//
+//                     </NavItem>
+//                     <NavItem eventKey={3}>
+//                       <Link to={"/boilerplates"}>Food Menu</Link>{" "}
+//
+//                     </NavItem>
+//                     <NavItem eventKey={5}>
+//                       <Link to={"/contact"}>Contact</Link>{" "}
+//
+//                     </NavItem>
+//                   </Nav>
+//               </Navbar>
+//             </header>
+//         )
+//     }
+// }
+//
+// export default Header;
--- src/components/Home.js
+++ src/components/Home.js
@@ -3,29 +3,10 @@ import {Carousel} from 'react-bootstrap';
 import '../styling/Home.css';
 
 class Home extends Component {
-    constructor(props) {
-      super(props);
-      this.state = {addClass: false}
-    }
-    handleClick() {
-        this.setState({addClass: !this.state.addClass});
-  }
     render () {
-        let clickedClass = ["beer"];
-        let clickedSmoke = ['explosion']
-        if(this.state.addClass) {
-            clickedClass.push('gone');
-            clickedSmoke.push('smoke');
-        }
         return (
             <div>
-                <h1 className="line-1 anim-typewriter">Welcome to the &lt;div&gt; bar</h1>
-                <div className="barWrapper">
-                    <img id="barPic" src={('/images/cartoonbar2.jpg')} />
-                    <img id="beer1" className={clickedClass.join(' ')} src={('/images/beer1.png')} onClick={(e) => this.handleClick(e)}/>
-                    <img id="explosion1" className={clickedSmoke.join(' ')}  src={('/images/explosion.png')}/>
-                    </div>
-
+                <h1 className="line-1 anim-typewriter">Welcome to the &lt;div&gt; bar></h1>
             </div>
         )
     }
