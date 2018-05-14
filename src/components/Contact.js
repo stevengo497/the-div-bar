397ec11d1d5c5d1fde6b9028868157930897726b
“stevengo497” <ngo.steve497@yahoo.com>
2018-05-14 14:51:53 -0600

    pics added to homepage

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,13 +1,13 @@
 import React, {Component} from 'react';
 import '../styling/Contact.css';
-
-
+import { Formio } from 'react-formio';
 
 class Contact extends Component {
     render () {
         return (
             <div>
                 <h1 className="line-1 anim-contact">&lt; Contact Us /&gt;</h1>
+
                 <div id="formio"></div>
             </div>
         )
--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,13 @@ class Home extends Component {
     render () {
         return (
             <div>
-                <h1 className="line-1 anim-typewriter">Welcome to the &lt;div&gt; bar></h1>
+                <h1 className="line-1 anim-typewriter">Welcome to the &lt;div&gt; bar</h1>
+                <div className="imageWrapper">
+                    <img id="cheersPic" src={'/images/cheers.jpeg'}/>
+                    <img id="newbarPic" src={'/images/newbar.jpeg'}/>
+                    <img id="winePic" src={'/images/wine.jpg'}/>
+                </div>
+
             </div>
         )
     }
